883e26401de568f1f00c7e70136bc7e40d8e3e0b
David Williams <david@david-williams.info>
2013-01-04 23:58:33 +0100

    Work on cubic LOD.

--- src/CubiquityUtility.cpp
+++ src/CubiquityUtility.cpp
@@ -17,12 +17,52 @@ void rescaleCubicVolume(RawVolume<Colour>* pVolSrc, const Region& regSrc, RawVol
 				Vector3DInt32 srcPos = regSrc.getLowerCorner() + (Vector3DInt32(x, y, z) * 2);
 				Vector3DInt32 dstPos = regDst.getLowerCorner() + Vector3DInt32(x, y, z);
 				RawVolume<Colour>::Sampler srcSampler(pVolSrc);
-				srcSampler.setPosition(srcPos);
+				//srcSampler.setPosition(srcPos);
 
-				if(srcSampler.getVoxel().getAlpha() > 0)
+				uint32_t noOfSolidVoxels = 0;
+				uint32_t totalRed = 0;
+				uint32_t totalGreen = 0;
+				uint32_t totalBlue = 0;
+				uint32_t totalExposedFaces = 0;
+
+				for(uint32_t childZ = 0; childZ < 2; childZ++)
+				{
+					for(uint32_t childY = 0; childY < 2; childY++)
+					{
+						for(uint32_t childX = 0; childX < 2; childX++)
+						{
+							srcSampler.setPosition(srcPos + Vector3DInt32(childX, childY, childZ));
+
+							Colour child = srcSampler.getVoxel();
+
+							if(child.getAlpha () > 0)
+							{
+								uint32_t exposedFaces = 0;
+								if(srcSampler.peekVoxel0px0py1nz().getAlpha() == 0) exposedFaces++;
+								if(srcSampler.peekVoxel0px0py1pz().getAlpha() == 0) exposedFaces++;
+								if(srcSampler.peekVoxel0px1ny0pz().getAlpha() == 0) exposedFaces++;
+								if(srcSampler.peekVoxel0px1py0pz().getAlpha() == 0) exposedFaces++;
+								if(srcSampler.peekVoxel1nx0py0pz().getAlpha() == 0) exposedFaces++;
+								if(srcSampler.peekVoxel1px0py0pz().getAlpha() == 0) exposedFaces++;
+
+								totalRed += child.getRed() * exposedFaces;
+								totalGreen += child.getGreen() * exposedFaces;
+								totalBlue += child.getBlue() * exposedFaces;
+
+								totalExposedFaces += exposedFaces;
+
+								noOfSolidVoxels++;
+							}							
+						}
+					}
+				}
+
+				if(totalExposedFaces == 0) totalExposedFaces++; //Avoid div by zero
+
+				if(noOfSolidVoxels == 8)
 				{
 					Colour colour;
-					colour.setColour(15,15,15,15);
+					colour.setColour(totalRed / totalExposedFaces, totalGreen / totalExposedFaces, totalBlue / totalExposedFaces, 15);
 					pVolDst->setVoxelAt(dstPos, colour);
 				}
 				else
