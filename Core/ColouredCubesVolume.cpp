fe18f0c522539628448addaee86641251865c8f6
David Williams <david@david-williams.info>
2013-01-29 23:42:52 +0100

    More work on coloured cubes resampling.

--- Core/ColouredCubesVolume.cpp
+++ Core/ColouredCubesVolume.cpp
@@ -8,6 +8,9 @@ void rescaleCubicVolume(RawVolume<Colour>* pVolSrc, const Region& regSrc, RawVol
 	POLYVOX_ASSERT(regSrc.getHeightInVoxels() == regDst.getHeightInVoxels() * 2, "Wrong size!");
 	POLYVOX_ASSERT(regSrc.getDepthInVoxels() == regDst.getDepthInVoxels() * 2, "Wrong size!");
 
+	RawVolume<Colour>::Sampler srcSampler(pVolSrc);
+	RawVolume<Colour>::Sampler dstSampler(pVolDst);
+
 	for(uint32_t z = 0; z < regDst.getDepthInVoxels(); z++)
 	{
 		for(uint32_t y = 0; y < regDst.getHeightInVoxels(); y++)
@@ -16,14 +19,11 @@ void rescaleCubicVolume(RawVolume<Colour>* pVolSrc, const Region& regSrc, RawVol
 			{
 				Vector3DInt32 srcPos = regSrc.getLowerCorner() + (Vector3DInt32(x, y, z) * 2);
 				Vector3DInt32 dstPos = regDst.getLowerCorner() + Vector3DInt32(x, y, z);
-				RawVolume<Colour>::Sampler srcSampler(pVolSrc);
-
-				Colour purple;
-				purple.setColour(16, 16, 0, 16);
-				srcSampler.setWrapMode(WrapModes::Border);
-				//srcSampler.setPosition(srcPos);
 
 				uint32_t noOfSolidVoxels = 0;
+				uint32_t averageOf8Red = 0;
+				uint32_t averageOf8Green = 0;
+				uint32_t averageOf8Blue = 0;
 				for(int32_t childZ = 0; childZ < 2; childZ++)
 				{
 					for(int32_t childY = 0; childY < 2; childY++)
@@ -37,6 +37,9 @@ void rescaleCubicVolume(RawVolume<Colour>* pVolSrc, const Region& regSrc, RawVol
 							if(child.getAlpha () > 0)
 							{
 								noOfSolidVoxels++;
+								averageOf8Red += child.getRed();
+								averageOf8Green += child.getGreen();
+								averageOf8Blue += child.getBlue();
 							}
 						}
 					}
@@ -44,45 +47,8 @@ void rescaleCubicVolume(RawVolume<Colour>* pVolSrc, const Region& regSrc, RawVol
 
 				if(noOfSolidVoxels > 7)
 				{
-					uint32_t totalRed = 0;
-					uint32_t totalGreen = 0;
-					uint32_t totalBlue = 0;
-					uint32_t totalExposedFaces = 0;
-
-					for(int32_t childZ = -1; childZ < 3; childZ++)
-					{
-						for(int32_t childY = -1; childY < 3; childY++)
-						{
-							for(int32_t childX = -1; childX < 3; childX++)
-							{
-								srcSampler.setPosition(srcPos + Vector3DInt32(childX, childY, childZ));
-
-								Colour child = srcSampler.getVoxel();
-
-								if(child.getAlpha () > 0)
-								{
-									uint32_t exposedFaces = 0;
-									if(srcSampler.peekVoxel0px0py1nz().getAlpha() == 0) exposedFaces++;
-									if(srcSampler.peekVoxel0px0py1pz().getAlpha() == 0) exposedFaces++;
-									if(srcSampler.peekVoxel0px1ny0pz().getAlpha() == 0) exposedFaces++;
-									if(srcSampler.peekVoxel0px1py0pz().getAlpha() == 0) exposedFaces++;
-									if(srcSampler.peekVoxel1nx0py0pz().getAlpha() == 0) exposedFaces++;
-									if(srcSampler.peekVoxel1px0py0pz().getAlpha() == 0) exposedFaces++;
-
-									totalRed += child.getRed() * exposedFaces;
-									totalGreen += child.getGreen() * exposedFaces;
-									totalBlue += child.getBlue() * exposedFaces;
-
-									totalExposedFaces += exposedFaces;
-								}							
-							}
-						}
-					}
-
-					if(totalExposedFaces == 0) totalExposedFaces++;
-
 					Colour colour;
-					colour.setColour(totalRed / totalExposedFaces, totalGreen / totalExposedFaces, totalBlue / totalExposedFaces, 15);
+					colour.setColour(averageOf8Red / noOfSolidVoxels, averageOf8Green / noOfSolidVoxels, averageOf8Blue / noOfSolidVoxels, 15);
 					pVolDst->setVoxelAt(dstPos, colour);
 				}
 				else
@@ -91,8 +57,73 @@ void rescaleCubicVolume(RawVolume<Colour>* pVolSrc, const Region& regSrc, RawVol
 					colour.setColour(0,0,0,0);
 					pVolDst->setVoxelAt(dstPos, colour);
 				}
+			}
+		}
+	}
 
+	for(uint32_t z = 0; z < regDst.getDepthInVoxels(); z++)
+	{
+		for(uint32_t y = 0; y < regDst.getHeightInVoxels(); y++)
+		{
+			for(uint32_t x = 0; x < regDst.getWidthInVoxels(); x++)
+			{
+				Vector3DInt32 dstPos = regDst.getLowerCorner() + Vector3DInt32(x, y, z);
 
+				dstSampler.setPosition(dstPos);
+
+				if(dstSampler.getVoxel().getAlpha() > 0)
+				{
+					if((dstSampler.peekVoxel0px0py1nz().getAlpha() == 0) ||
+					   (dstSampler.peekVoxel0px0py1pz().getAlpha() == 0) || 
+					   (dstSampler.peekVoxel0px1ny0pz().getAlpha() == 0) ||
+					   (dstSampler.peekVoxel0px1py0pz().getAlpha() == 0) ||
+					   (dstSampler.peekVoxel1nx0py0pz().getAlpha() == 0) || 
+					   (dstSampler.peekVoxel1px0py0pz().getAlpha() == 0))
+					{
+						Vector3DInt32 srcPos = regSrc.getLowerCorner() + (Vector3DInt32(x, y, z) * 2);
+
+						uint32_t totalRed = 0;
+						uint32_t totalGreen = 0;
+						uint32_t totalBlue = 0;
+						uint32_t totalExposedFaces = 0;
+
+						for(int32_t childZ = -1; childZ < 3; childZ++)
+						{
+							for(int32_t childY = -1; childY < 3; childY++)
+							{
+								for(int32_t childX = -1; childX < 3; childX++)
+								{
+									srcSampler.setPosition(srcPos + Vector3DInt32(childX, childY, childZ));
+
+									Colour child = srcSampler.getVoxel();
+
+									if(child.getAlpha () > 0)
+									{
+										uint32_t exposedFaces = 0;
+										if(srcSampler.peekVoxel0px0py1nz().getAlpha() == 0) exposedFaces++;
+										if(srcSampler.peekVoxel0px0py1pz().getAlpha() == 0) exposedFaces++;
+										if(srcSampler.peekVoxel0px1ny0pz().getAlpha() == 0) exposedFaces++;
+										if(srcSampler.peekVoxel0px1py0pz().getAlpha() == 0) exposedFaces++;
+										if(srcSampler.peekVoxel1nx0py0pz().getAlpha() == 0) exposedFaces++;
+										if(srcSampler.peekVoxel1px0py0pz().getAlpha() == 0) exposedFaces++;
+
+										totalRed += child.getRed() * exposedFaces;
+										totalGreen += child.getGreen() * exposedFaces;
+										totalBlue += child.getBlue() * exposedFaces;
+
+										totalExposedFaces += exposedFaces;
+									}							
+								}
+							}
+						}
+
+						if(totalExposedFaces == 0) totalExposedFaces++;
+
+						Colour colour;
+						colour.setColour(totalRed / totalExposedFaces, totalGreen / totalExposedFaces, totalBlue / totalExposedFaces, 15);
+						pVolDst->setVoxelAt(dstPos, colour);
+					}
+				}
 			}
 		}
 	}
