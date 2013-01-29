eee87cdb9a83fd40e1f61d757d6b8a0f6fff7ad8
David Williams <david@david-williams.info>
2013-01-29 21:27:09 +0100

    More work on cubic volume resampling.

--- Core/ColouredCubesVolume.cpp
+++ Core/ColouredCubesVolume.cpp
@@ -24,11 +24,6 @@ void rescaleCubicVolume(RawVolume<Colour>* pVolSrc, const Region& regSrc, RawVol
 				//srcSampler.setPosition(srcPos);
 
 				uint32_t noOfSolidVoxels = 0;
-				uint32_t totalRed = 0;
-				uint32_t totalGreen = 0;
-				uint32_t totalBlue = 0;
-				uint32_t totalExposedFaces = 0;
-
 				for(int32_t childZ = 0; childZ < 2; childZ++)
 				{
 					for(int32_t childY = 0; childY < 2; childY++)
@@ -41,30 +36,51 @@ void rescaleCubicVolume(RawVolume<Colour>* pVolSrc, const Region& regSrc, RawVol
 
 							if(child.getAlpha () > 0)
 							{
-								uint32_t exposedFaces = 0;
-								if(srcSampler.peekVoxel0px0py1nz().getAlpha() == 0) exposedFaces++;
-								if(srcSampler.peekVoxel0px0py1pz().getAlpha() == 0) exposedFaces++;
-								if(srcSampler.peekVoxel0px1ny0pz().getAlpha() == 0) exposedFaces++;
-								if(srcSampler.peekVoxel0px1py0pz().getAlpha() == 0) exposedFaces++;
-								if(srcSampler.peekVoxel1nx0py0pz().getAlpha() == 0) exposedFaces++;
-								if(srcSampler.peekVoxel1px0py0pz().getAlpha() == 0) exposedFaces++;
-
-								totalRed += child.getRed() * exposedFaces;
-								totalGreen += child.getGreen() * exposedFaces;
-								totalBlue += child.getBlue() * exposedFaces;
-
-								totalExposedFaces += exposedFaces;
-
 								noOfSolidVoxels++;
-							}							
+							}
 						}
 					}
 				}
 
-				if(totalExposedFaces == 0) totalExposedFaces++; //Avoid div by zero
-
-				if(noOfSolidVoxels > 3)
+				if(noOfSolidVoxels > 7)
 				{
+					uint32_t totalRed = 0;
+					uint32_t totalGreen = 0;
+					uint32_t totalBlue = 0;
+					uint32_t totalExposedFaces = 0;
+
+					for(int32_t childZ = -1; childZ < 3; childZ++)
+					{
+						for(int32_t childY = -1; childY < 3; childY++)
+						{
+							for(int32_t childX = -1; childX < 3; childX++)
+							{
+								srcSampler.setPosition(srcPos + Vector3DInt32(childX, childY, childZ));
+
+								Colour child = srcSampler.getVoxel();
+
+								if(child.getAlpha () > 0)
+								{
+									uint32_t exposedFaces = 0;
+									if(srcSampler.peekVoxel0px0py1nz().getAlpha() == 0) exposedFaces++;
+									if(srcSampler.peekVoxel0px0py1pz().getAlpha() == 0) exposedFaces++;
+									if(srcSampler.peekVoxel0px1ny0pz().getAlpha() == 0) exposedFaces++;
+									if(srcSampler.peekVoxel0px1py0pz().getAlpha() == 0) exposedFaces++;
+									if(srcSampler.peekVoxel1nx0py0pz().getAlpha() == 0) exposedFaces++;
+									if(srcSampler.peekVoxel1px0py0pz().getAlpha() == 0) exposedFaces++;
+
+									totalRed += child.getRed() * exposedFaces;
+									totalGreen += child.getGreen() * exposedFaces;
+									totalBlue += child.getBlue() * exposedFaces;
+
+									totalExposedFaces += exposedFaces;
+								}							
+							}
+						}
+					}
+
+					if(totalExposedFaces == 0) totalExposedFaces++;
+
 					Colour colour;
 					colour.setColour(totalRed / totalExposedFaces, totalGreen / totalExposedFaces, totalBlue / totalExposedFaces, 15);
 					pVolDst->setVoxelAt(dstPos, colour);
@@ -75,6 +91,8 @@ void rescaleCubicVolume(RawVolume<Colour>* pVolSrc, const Region& regSrc, RawVol
 					colour.setColour(0,0,0,0);
 					pVolDst->setVoxelAt(dstPos, colour);
 				}
+
+
 			}
 		}
 	}
