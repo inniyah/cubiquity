4b1d460f7b3b077b32e64fddc2eba0c801cb8459
David Williams <david@david-williams.info>
2013-02-02 20:54:03 +0100

    Work on heightmap converter.

--- Tools/HeightmapConverter/HeightmapConverter/main.cpp
+++ Tools/HeightmapConverter/HeightmapConverter/main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
 
+#include "ColouredCubesVolume.h"
+#include "VolumeSerialisation.h"
+
+#include "stb_image.c"
+
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	cout << "Hello" <<endl;
+	int imageWidth, imageHeight, n;
+	uint32_t imageRange = 256;
+	unsigned char *heightData = stbi_load("inputSmall.png", &imageWidth, &imageHeight, &n, 0);
+
+	int colourImageWidth, colourImageHeight, colourN;
+	unsigned char *colourData = stbi_load("colour.png", &colourImageWidth, &colourImageHeight, &colourN, 0);
+
+	ColouredCubesVolume volume(0, 0, 0, imageWidth-1, imageRange-1, imageHeight-1, 32, 32);
+	for(uint32_t x = 0; x < imageWidth; x++)
+	{
+		cout << x << endl;
+		for(uint32_t y = 0; y < imageHeight; y++)
+		{
+			unsigned char heightmapValue = *(heightData + (y * imageWidth + x));
+			unsigned char* colourmapValue = colourData + (y * imageWidth + x) * colourN;
+			unsigned char red = *colourmapValue;
+			unsigned char green = *(colourmapValue + 1);
+			unsigned char blue = *(colourmapValue + 2);
+			for(uint32_t r = 0; r < imageRange; r++) //Note that we traverse y last.
+			{
+				Colour voxel;
+				if(r <= heightmapValue)
+				{
+					voxel.setColour(red / 16, green / 16, blue / 16, 15);
+				}
+				else
+				{
+					voxel.setColour(0, 0, 0, 0);
+				}
+				volume.setVoxelAt(x, r, y, voxel); //Note suprising order.
+			}
+		}
+	}
+
+	saveData("output.vol", &volume);
+
+	return 0;
 }
