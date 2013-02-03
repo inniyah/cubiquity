cc43fda213a8bd0780d419dc553d40d01d760c47
David Williams <david@david-williams.info>
2013-02-03 21:23:08 +0100

    Added underground layers to heightmap converter.

--- Tools/HeightmapConverter/HeightmapConverter/main.cpp
+++ Tools/HeightmapConverter/HeightmapConverter/main.cpp
@@ -11,10 +11,16 @@ int main(int argc, char** argv)
 {
 	int imageWidth, imageHeight, n;
 	uint32_t imageRange = 256;
-	unsigned char *heightData = stbi_load("input.png", &imageWidth, &imageHeight, &n, 0);
+	unsigned char *heightData = stbi_load("height.png", &imageWidth, &imageHeight, &n, 0);
 
 	int colourImageWidth, colourImageHeight, colourN;
 	unsigned char *colourData = stbi_load("colour.png", &colourImageWidth, &colourImageHeight, &colourN, 0);
+
+	Colour topLayer;
+	topLayer.setColour(7, 3, 0, 15);
+
+	Colour bottomLayer;
+	bottomLayer.setColour(3, 3, 3, 15);
 
 	ColouredCubesVolume volume(0, 0, 0, imageWidth-1, imageRange-1, imageHeight-1, 32, 32);
 	for(uint32_t x = 0; x < imageWidth; x++)
@@ -30,7 +36,15 @@ int main(int argc, char** argv)
 			for(uint32_t r = 0; r < imageRange; r++) //Note that we traverse y last.
 			{
 				Colour voxel;
-				if(r <= heightmapValue)
+				if(r <= heightmapValue - 10)
+				{
+					voxel = bottomLayer;
+				}
+				else if(r <= heightmapValue - 3)
+				{
+					voxel = topLayer;
+				}
+				else if(r <= heightmapValue)
 				{
 					voxel.setColour(red / 16, green / 16, blue / 16, 15);
 				}
