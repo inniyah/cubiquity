d44fd0812a73b26f303232fe2be9fd6a2971dcf1
David Williams <david@volumesoffun.com>
2013-02-18 00:05:25 +0100

    Work on vxl loading.

--- Examples/gameplay/Lua/VxlEditor/VxlSerialisation.lua
+++ Examples/gameplay/Lua/VxlEditor/VxlSerialisation.lua
@@ -6,14 +6,14 @@ function importVxl()
 	result = GameplayColouredCubesVolume.create(0, 0, 0, volumeWidth - 1, volumeHeight - 1, volumeDepth - 1, 64, 64)
 
 	local colour = Vector4.new(0.9, 0.9, 0.9, 1.0)
-	local empty = Vector4.new(0.0, 0.0, 0.0, 0.0)
-	for iZ=0, volumeWidth - 1 do
-		for iY=0, 1 do
-			for iX=0, volumeDepth - 1 do
-				result:setVoxel(iX, iY, iZ, colour, false)
-			end
-		end
-	end
+	--local empty = Vector4.new(0.0, 0.0, 0.0, 0.0)
+	--for iZ=0, volumeWidth - 1 do
+	--	for iY=0, 1 do
+	--		for iX=0, volumeDepth - 1 do
+	--			result:setVoxel(iX, iY, iZ, colour, false)
+	--		end
+	--	end
+	--end
 	result:markRegionAsModified(0, 0, 0, volumeWidth - 1, volumeHeight - 1, volumeDepth - 1)
 
 
@@ -30,10 +30,10 @@ function importVxl()
 	i = 1
 	x = 0
 	y = 0
-	--columnI = 0
+	columnI = 0
 	mapSize = 512
 	columnCount = mapSize * mapSize
-	for columnI = 0, columnCount do
+	while columnI < columnCount do
 		N = data:byte(i)
 		S = data:byte(i + 1)
 		E = data:byte(i + 2)
@@ -65,11 +65,13 @@ function importVxl()
 				blue = data:byte(i + 4 + colorI * 4)
 				-- Do something with these colors
 				-- makeVoxelColorful(x, y, zz, red, green, blue);
+				--local myColour = Vector4.new(red / 255.0, green / 255.0, blue / 255.0, 1.0)
 				result:setVoxel(x, zz-1, y, colour, false)
 				zz = zz + 1
 				colorI = colorI + 1
 			end
 		end
+
 		-- Now deal with solid non-surface voxels
 		-- No color data is provided for non-surface voxels
 		zz = E + 1
@@ -83,16 +85,17 @@ function importVxl()
 			columnI = columnI + 1
 			x = x + 1
 			if (x >= mapSize) then
+				print(columnI)
 				x = 0;
 				y = y + 1
+				
+				print(y)
 			end
 			i = i + (4*(1 + K))
 		else
 			i = i + (N * 4)
 		end
 	end
-
-
 
 	return result
 end
