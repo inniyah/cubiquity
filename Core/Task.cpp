f38dcccb0aa1e67c32a25c85c098e74a376c06e0
David Williams <david@david-williams.info>
2013-03-01 00:02:23 +0100

    Adding state to Task class.

---
+++ Core/Task.cpp
@@ -0,0 +1,33 @@
+#include "Task.h"
+
+TaskState& operator++(TaskState& taskState)
+{
+	int tmp = static_cast<int>(taskState);
+	++tmp;
+	taskState = static_cast<TaskState>(tmp);
+	return taskState;
+}
+
+TaskState operator++(TaskState& taskState, int)
+{
+  TaskState tmp(taskState);
+  ++taskState;
+  return tmp;
+}
+
+Task::Task()
+	:mState(TaskStates::Pending)
+{
+}
+
+TaskState Task::getState(void)
+{
+	return mState;
+}
+
+void Task::gotoNextState(void)
+{
+	// NEED MUTEX HERE?
+	POLYVOX_ASSERT(mState != TaskStates::Finished, "Already on the finished state!");
+	mState++;
+}
--- Core/Task.h
+++ Core/Task.h
@@ -1,13 +1,34 @@
 #ifndef CUBIQUITY_TASK_H_
 #define CUBIQUITY_TASK_H_
 
+#include "PolyVoxCore/Impl/ErrorHandling.h"
+
+namespace TaskStates
+{
+	enum TaskState
+	{
+		Pending,
+		Processing,
+		Finished
+	};
+}
+typedef TaskStates::TaskState TaskState;
+
+TaskState& operator++(TaskState& taskState);
+TaskState operator++(TaskState& taskState, int);
+
 class Task
 {
 public:
-	Task() {}
-	virtual ~Task() {}
+	Task();
+
+	TaskState getState(void);
+	void gotoNextState(void);
 
 	virtual void process(void) = 0;
+
+private:
+	TaskState mState;
 };
 
 #endif //CUBIQUITY_TASK_H_
