60e9c560e39f4907677f4df87a3068663512d000
David Williams <david@volumesoffun.com>
2013-03-11 17:16:38 +0100

    Heightmap converter now output result in slice format.

--- Tools/HeightmapConverter/HeightmapConverter/main.cpp
+++ Tools/HeightmapConverter/HeightmapConverter/main.cpp
@@ -1,9 +1,12 @@
+#include <algorithm>
+#include <cstdint>
 #include <iostream>
-
-#include "ColouredCubesVolume.h"
-#include "VolumeSerialisation.h"
+#include <sstream>
 
 #include "stb_image.c"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 using namespace std;
 
@@ -11,53 +14,77 @@ int main(int argc, char** argv)
 {
 	int imageWidth, imageHeight, n;
 	uint32_t imageRange = 256;
+	uint32_t outputChannels = 4;
 	unsigned char *heightData = stbi_load("height.png", &imageWidth, &imageHeight, &n, 0);
 
 	int colourImageWidth, colourImageHeight, colourN;
 	unsigned char *colourData = stbi_load("colour.png", &colourImageWidth, &colourImageHeight, &colourN, 0);
 
-	Colour topLayer;
-	topLayer.setColour(7, 3, 0, 15);
+	int outputSliceDataSize = imageWidth * imageHeight * outputChannels;
+	unsigned char* outputSliceData = new unsigned char[outputSliceDataSize];
 
-	Colour bottomLayer;
-	bottomLayer.setColour(3, 3, 3, 15);
-
-	ColouredCubesVolume volume(0, 0, 0, imageWidth-1, imageRange-1, imageHeight-1, 32, 32);
-	for(uint32_t x = 0; x < imageWidth; x++)
+	for(uint32_t z = 0; z < imageRange; z++)
 	{
-		cout << x << endl;
-		for(uint32_t y = 0; y < imageHeight; y++)
+		cout << z << endl;
+
+		std::fill(outputSliceData, outputSliceData + imageWidth * imageHeight, 0);
+
+		bool anyPixelsSet = false;
+
+		for(uint32_t x = 0; x < imageWidth; x++)
 		{
-			unsigned char heightmapValue = *(heightData + (y * imageWidth + x) * n);
-			unsigned char* colourmapValue = colourData + (y * imageWidth + x) * colourN;
-			unsigned char red = *colourmapValue;
-			unsigned char green = *(colourmapValue + 1);
-			unsigned char blue = *(colourmapValue + 2);
-			for(uint32_t r = 0; r < imageRange; r++) //Note that we traverse y last.
+			for(uint32_t y = 0; y < imageHeight; y++)
 			{
-				Colour voxel;
-				if(r <= heightmapValue - 10)
+				int heightmapValue = *(heightData + (y * imageWidth + x) * n);
+				unsigned char* colourmapValue = colourData + (y * imageWidth + x) * colourN;
+				unsigned char* outputSliceDataValue = outputSliceData + (y * imageWidth + x) * outputChannels;
+				unsigned char red = *colourmapValue;
+				unsigned char green = *(colourmapValue + 1);
+				unsigned char blue = *(colourmapValue + 2);
+
+				unsigned char outputRed = 0, outputGreen = 0, outputBlue = 0, outputAlpha = 0;
+
+				if(static_cast<int>(z) <= heightmapValue - 5)
 				{
-					voxel = bottomLayer;
+					outputRed = 127;
+					outputGreen = 127;
+					outputBlue = 127;
+					outputAlpha = 255;
+					anyPixelsSet = true;
 				}
-				else if(r <= heightmapValue - 3)
+				else if(z <= heightmapValue)
 				{
-					voxel = topLayer;
+					outputRed = red;
+					outputGreen = green;
+					outputBlue = blue;
+					outputAlpha = 255;
+					anyPixelsSet = true;
 				}
-				else if(r <= heightmapValue)
+
+				if(anyPixelsSet)
 				{
-					voxel.setColour(red / 16, green / 16, blue / 16, 15);
+					*outputSliceDataValue = outputRed;
+					*(outputSliceDataValue+1) = outputGreen;
+					*(outputSliceDataValue+2) = outputBlue;
+					*(outputSliceDataValue+3) = outputAlpha;
 				}
-				else
-				{
-					voxel.setColour(0, 0, 0, 0);
-				}
-				volume.setVoxelAt(x, r, y, voxel); //Note suprising order.
 			}
+		}
+
+		stringstream ss;
+		ss << "C:/temp/output/" << z << ".png";
+		int result = stbi_write_png(ss.str().c_str(), imageWidth, imageHeight, outputChannels, outputSliceData, imageWidth * outputChannels);
+		assert(result); //If crashing here then make sure the output folder exists.
+
+		// We're processing a heightmap, so one we have a completely
+		// empty slice we know there is no more data higher up
+		if(!anyPixelsSet)
+		{
+			break;
 		}
 	}
 
-	saveData("output.vol", &volume);
+	delete[] outputSliceData;
 
 	return 0;
 }
