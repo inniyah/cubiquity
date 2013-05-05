b00f26363e2dde07f4693668bba0c792217534e4
David Williams <david@david-williams.info>
2013-05-05 23:31:51 +0200

    Moved some code.

--- bridges/gameplay/Impl/GameplayVolume.h
+++ bridges/gameplay/Impl/GameplayVolume.h
@@ -91,78 +91,7 @@ namespace Cubiquity
 
 		virtual gameplay::Model* buildModelFromPolyVoxMesh(const PolyVox::SurfaceMesh< typename VoxelTraits<typename CubiquityVolumeType::VoxelType>::VertexType>* polyVoxMesh) = 0;
 
-		void syncNode(OctreeNode< typename CubiquityVolumeType::VoxelType >* octreeNode, GameplayOctreeNode* gameplayOctreeNode)
-		{
-			if(gameplayOctreeNode->mTimeStamp < octreeNode->mMeshLastUpdated)
-			{
-				if(octreeNode->mPolyVoxMesh)
-				{
-					Model* model = buildModelFromPolyVoxMesh(octreeNode->mPolyVoxMesh);
-					if(VoxelTraits<typename CubiquityVolumeType::VoxelType>::IsColour)
-					{
-						model->setMaterial("res/Materials/ColouredCubicTerrain.material");
-					}
-					else
-					{
-						model->setMaterial("res/Materials/SmoothTerrain.material");
-					}
-					gameplayOctreeNode->mGameplayNode->setModel(model);
-					SAFE_RELEASE(model);
-
-					//There is a weird bug, whereby if we use the LOD 0 for physics it resets the node positions somehow. So we use LOD 1 here.
-					//if(octreeNode->mHeight == 0)
-					{
-						PhysicsCollisionShape::Definition physDef = buildCollisionObjectFromPolyVoxMesh(octreeNode->mPolyVoxMesh);
-
-						PhysicsRigidBody::Parameters groundParams;
-						groundParams.mass = 0.0f;
-						gameplayOctreeNode->mGameplayNode->setCollisionObject(PhysicsCollisionObject::RIGID_BODY, physDef, &groundParams);
-					}
-				}	
-
-				gameplayOctreeNode->mTimeStamp = Clock::getTimestamp();
-			}
-
-			if(octreeNode->mRenderThisNode)
-			{
-				gameplayOctreeNode->mGameplayNode->setTag("RenderThisNode", "t");
-			}
-			else
-			{
-				gameplayOctreeNode->mGameplayNode->setTag("RenderThisNode", "f");
-			}
-
-			for(int iz = 0; iz < 2; iz++)
-			{
-				for(int iy = 0; iy < 2; iy++)
-				{
-					for(int ix = 0; ix < 2; ix++)
-					{
-						OctreeNode< typename CubiquityVolumeType::VoxelType >* child = octreeNode->getChildNode(ix, iy, iz);
-						if(child)
-						{
-							//Node* childNode = reinterpret_cast<Node*>(child->mGameEngineNode);
-							GameplayOctreeNode* childGameplayOctreeNode = gameplayOctreeNode->mChildren[ix][iy][iz];
-
-							if(childGameplayOctreeNode == 0)
-							{
-								childGameplayOctreeNode = new GameplayOctreeNode();
-								childGameplayOctreeNode->mGameplayNode = gameplay::Node::create();
-
-								Vector3I translation = child->mRegion.getLowerCorner() - octreeNode->mRegion.getLowerCorner();
-								childGameplayOctreeNode->mGameplayNode->setTranslation(translation.getX(), translation.getY(), translation.getZ());
-
-								gameplayOctreeNode->mChildren[ix][iy][iz] = childGameplayOctreeNode;
-
-								gameplayOctreeNode->mGameplayNode->addChild(childGameplayOctreeNode->mGameplayNode);
-							}
-
-							syncNode(child, childGameplayOctreeNode);
-						}
-					}
-				}
-			}
-		}
+		void syncNode(OctreeNode< typename CubiquityVolumeType::VoxelType >* octreeNode, GameplayOctreeNode* gameplayOctreeNode);
 
 	protected:
 
