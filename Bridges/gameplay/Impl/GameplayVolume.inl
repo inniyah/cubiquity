b4a43f7b0aeba5bdc7a287778760ce867cc7d5ea
David Williams <david@david-williams.info>
2013-05-05 22:58:12 +0200

    Tidied some code.

---
+++ Bridges/gameplay/Impl/GameplayVolume.inl
@@ -0,0 +1,45 @@
+namespace Cubiquity
+{
+	template <typename VolumeType>
+	void GameplayVolume<VolumeType>::initialiseOctree(void)
+	{
+		GP_ASSERT(mCubiquityVolume);
+		GP_ASSERT(mCubiquityVolume->getRootOctreeNode());
+
+		mRootGameplayOctreeNode = new GameplayOctreeNode();
+		mRootGameplayOctreeNode->mGameplayNode = Node::create();
+
+		std::stringstream ss;
+		ss << "LOD = " << int(mCubiquityVolume->getRootOctreeNode()->mHeight) << ", Region = (" << mCubiquityVolume->getRootOctreeNode()->mRegion.getLowerX() << "," << mCubiquityVolume->getRootOctreeNode()->mRegion.getLowerY() << "," << mCubiquityVolume->getRootOctreeNode()->mRegion.getLowerZ() << ") to (" << mCubiquityVolume->getRootOctreeNode()->mRegion.getUpperX() << "," << mCubiquityVolume->getRootOctreeNode()->mRegion.getUpperY() << "," << mCubiquityVolume->getRootOctreeNode()->mRegion.getUpperZ() << ")";
+		mRootGameplayOctreeNode->mGameplayNode->setId(ss.str().c_str());
+
+		Vector3I translation = mCubiquityVolume->getRootOctreeNode()->mRegion.getLowerCorner();
+		mRootGameplayOctreeNode->mGameplayNode->setTranslation(translation.getX(), translation.getY(), translation.getZ());
+	}
+
+	template <typename _VolumeType>
+	gameplay::PhysicsCollisionShape::Definition GameplayVolume<_VolumeType>::buildCollisionObjectFromPolyVoxMesh(const PolyVox::SurfaceMesh< typename VoxelTraits<typename _VolumeType::VoxelType>::VertexType>* polyVoxMesh)
+	{
+		//Now set up the physics
+		const std::vector< typename VoxelTraits<typename VolumeType::VoxelType>::VertexType >& vecVertices = polyVoxMesh->getVertices();
+		const std::vector<unsigned int>& vecIndices = polyVoxMesh->getIndices();
+		float* vertexData = new float[polyVoxMesh->getVertices().size() * 3];
+
+		unsigned int* physicsIndices = new unsigned int [vecIndices.size()];
+		for(uint32_t ct = 0; ct < vecIndices.size(); ct++)
+		{
+			physicsIndices[ct] = vecIndices[ct];
+		}
+
+		float* ptr = vertexData;
+		for(uint32_t i = 0; i < vecVertices.size(); i++)
+		{
+			// Position stored in x,y,z components.
+			*ptr = vecVertices[i].getPosition().getX(); ptr++;
+			*ptr = vecVertices[i].getPosition().getY(); ptr++;
+			*ptr = vecVertices[i].getPosition().getZ(); ptr++;
+		}
+
+		return PhysicsCollisionShape::custom(vertexData, polyVoxMesh->getVertices().size(), physicsIndices, vecIndices.size());
+	}
+}
--- bridges/gameplay/Impl/GameplayVolume.h
+++ bridges/gameplay/Impl/GameplayVolume.h
@@ -85,46 +85,9 @@ namespace Cubiquity
 		{
 		}
 
-		void initialiseOctree(void)
-		{
-			GP_ASSERT(mCubiquityVolume);
-			GP_ASSERT(mCubiquityVolume->getRootOctreeNode());
+		void initialiseOctree(void);
 
-			mRootGameplayOctreeNode = new GameplayOctreeNode();
-			mRootGameplayOctreeNode->mGameplayNode = Node::create();
-
-			std::stringstream ss;
-			ss << "LOD = " << int(mCubiquityVolume->getRootOctreeNode()->mHeight) << ", Region = (" << mCubiquityVolume->getRootOctreeNode()->mRegion.getLowerX() << "," << mCubiquityVolume->getRootOctreeNode()->mRegion.getLowerY() << "," << mCubiquityVolume->getRootOctreeNode()->mRegion.getLowerZ() << ") to (" << mCubiquityVolume->getRootOctreeNode()->mRegion.getUpperX() << "," << mCubiquityVolume->getRootOctreeNode()->mRegion.getUpperY() << "," << mCubiquityVolume->getRootOctreeNode()->mRegion.getUpperZ() << ")";
-			mRootGameplayOctreeNode->mGameplayNode->setId(ss.str().c_str());
-
-			Vector3I translation = mCubiquityVolume->getRootOctreeNode()->mRegion.getLowerCorner();
-			mRootGameplayOctreeNode->mGameplayNode->setTranslation(translation.getX(), translation.getY(), translation.getZ());
-		}
-
-		gameplay::PhysicsCollisionShape::Definition buildCollisionObjectFromPolyVoxMesh(const PolyVox::SurfaceMesh< typename VoxelTraits<typename _VolumeType::VoxelType>::VertexType>* polyVoxMesh)
-		{
-			//Now set up the physics
-			const std::vector< typename VoxelTraits<typename _VolumeType::VoxelType>::VertexType >& vecVertices = polyVoxMesh->getVertices();
-			const std::vector<unsigned int>& vecIndices = polyVoxMesh->getIndices();
-			float* vertexData = new float[polyVoxMesh->getVertices().size() * 3];
-
-			unsigned int* physicsIndices = new unsigned int [vecIndices.size()];
-			for(uint32_t ct = 0; ct < vecIndices.size(); ct++)
-			{
-				physicsIndices[ct] = vecIndices[ct];
-			}
-
-			float* ptr = vertexData;
-			for(uint32_t i = 0; i < vecVertices.size(); i++)
-			{
-				// Position stored in x,y,z components.
-				*ptr = vecVertices[i].getPosition().getX(); ptr++;
-				*ptr = vecVertices[i].getPosition().getY(); ptr++;
-				*ptr = vecVertices[i].getPosition().getZ(); ptr++;
-			}
-
-			return PhysicsCollisionShape::custom(vertexData, polyVoxMesh->getVertices().size(), physicsIndices, vecIndices.size());
-		}
+		gameplay::PhysicsCollisionShape::Definition buildCollisionObjectFromPolyVoxMesh(const PolyVox::SurfaceMesh< typename VoxelTraits<typename _VolumeType::VoxelType>::VertexType>* polyVoxMesh);
 
 		virtual gameplay::Model* buildModelFromPolyVoxMesh(const PolyVox::SurfaceMesh< typename VoxelTraits<typename VolumeType::VoxelType>::VertexType>* polyVoxMesh) = 0;
 
@@ -207,5 +170,7 @@ namespace Cubiquity
 		GameplayOctreeNode* mRootGameplayOctreeNode;
 	};
 }
+
+#include "GameplayVolume.inl"
 
 #endif //GAMEPLAYVOLUME_H_
