33e5008e8d79c0ef57386672a9dfbd0fa4664613
Daviw Williams <david@volumesoffun.com>
2013-05-23 15:16:40 +0200

    Work on Cubiquity logging system.

--- Core/Logging.cpp
+++ Core/Logging.cpp
@@ -1,5 +1,7 @@
 #include "Logging.h"
 
+#include "PolyVoxCore/Impl/ErrorHandling.h"
+
 #include <boost/log/expressions.hpp>
 #include <boost/log/sources/severity_logger.hpp>
 #include <boost/log/sources/record_ostream.hpp>
@@ -10,18 +12,21 @@
 
 namespace Cubiquity
 {
+	//BOOST_LOG_GLOBAL_LOGGER(my_logger, boost::log::sources::severity_logger_mt< boost::log::trivial::severity_level >)
+
 	BOOST_LOG_INLINE_GLOBAL_LOGGER_INIT(my_logger, boost::log::sources::severity_logger_mt< boost::log::trivial::severity_level >)
 	{
 		boost::log::add_file_log
 		(
-			boost::log::keywords::file_name = "Cubiquity.log",
+			boost::log::keywords::file_name = "CubiquityOutput.log",
 			boost::log::keywords::format =
 			(
 				boost::log::expressions::stream
 					<< "[" << boost::log::trivial::severity << "]" 
 					<< "<" << boost::log::expressions::attr< unsigned int >("ThreadID") << ">: "
 					<< boost::log::expressions::smessage
-			)
+			),
+			boost::log::keywords::auto_flush = true
 		);
 
 		boost::log::add_common_attributes();
@@ -30,9 +35,58 @@ namespace Cubiquity
 		return lg;
 	}
 
+	class BoostLogStream : public std::ostream
+	{
+	private:
+		class BoostLogBuf : public std::stringbuf
+		{
+		private:
+			// or whatever you need for your application
+			boost::log::trivial::severity_level mSeverityLevel;
+		public:
+			BoostLogBuf(boost::log::trivial::severity_level severityLevel) : mSeverityLevel(severityLevel) { }
+			~BoostLogBuf() {  /*pubsync();*/ }
+			int sync()
+			{
+				boost::log::sources::severity_logger_mt< boost::log::trivial::severity_level >& lg = my_logger::get();
+				BOOST_LOG_SEV(lg, mSeverityLevel) << str();
+				str("");
+				return 0;
+			}
+		};
+
+	public:
+		// Other constructors could specify filename, etc
+		// just remember to pass whatever you need to CLogBuf
+		BoostLogStream(boost::log::trivial::severity_level severityLevel) : std::ostream(new BoostLogBuf(severityLevel)) {}
+		~BoostLogStream() { delete rdbuf(); }
+	};
+
+	BoostLogStream fatalStream(boost::log::trivial::fatal);
+
+	//BOOST_LOG_GLOBAL_LOGGER_INIT(my_logger, boost::log::sources::severity_logger_mt< boost::log::trivial::severity_level >)
+	
+
 	void logMessage(const std::string& message)
 	{
 		boost::log::sources::severity_logger_mt< boost::log::trivial::severity_level >& lg = my_logger::get();
-		BOOST_LOG_SEV(lg, boost::log::trivial::info) << message;
+		BOOST_LOG_SEV(lg, boost::log::trivial::info) << message << std::flush;
 	}
+
+	class EntryAndExitPoints
+	{
+	public:
+		EntryAndExitPoints()
+		{
+			std::cout << "Starting Cubiquity" << std::endl;
+			PolyVox::setFatalStream(&fatalStream);
+		}
+
+		~EntryAndExitPoints()
+		{
+			std::cout << "Exiting Cubiquity" << std::endl;
+		}
+	};
+
+	EntryAndExitPoints gEntryAndExitPoint;
 }
