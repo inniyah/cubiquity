09b6246bff2facfb2ff656082509bd6fe41b5aba
David Williams <david@volumesoffun.com>
2013-06-12 23:07:13 +0200

    Work on the way cubes are spawned for explosions.

--- Examples/Unity/Test DLL/Assets/Bullet.cs
+++ Examples/Unity/Test DLL/Assets/Bullet.cs
@@ -47,9 +47,18 @@ public class Bullet : MonoBehaviour
 	
 	void OnCollisionEnter(Collision collision)
 	{
-		int xPos = (int)transform.position.x;
-		int yPos = (int)transform.position.y;
-		int zPos = (int)transform.position.z;	
+		//Vector3 explosionCentre = transform.position + (rigidbody.velocity * 1.0f);
+		
+		ContactPoint contactPoint = collision.contacts[0];
+		Vector3 contactPosition = contactPoint.point;
+		
+		int xPos = (int)(contactPosition.x + 0.5f);
+		int yPos = (int)(contactPosition.y + 0.5f);
+		int zPos = (int)(contactPosition.z + 0.5f);	
+		
+		//Vector3 forceDirection = cube.transform.position - collision.;
+						
+		//Debug.Log(forceDirection);
 		
 		Color32 emptyVoxelColor = new Color32(0, 0, 0, 0);
 			
@@ -67,8 +76,11 @@ public class Bullet : MonoBehaviour
 						GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 						cube.AddComponent<Rigidbody>();
 						cube.transform.position = new Vector3(x, y, z);
-						cube.transform.localScale = new Vector3(0.99f, 0.99f, 0.99f);
-						cube.renderer.material.color = color;
+						cube.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+						cube.renderer.material.color = color;						
+						
+						cube.rigidbody.AddTorque(12.0f, 21.3f, 13.4f);
+						cube.rigidbody.AddForce(contactPoint.normal * 100.0f);
 					}
 				}
 			}
