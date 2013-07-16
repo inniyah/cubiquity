bc7455cfdd9faa39619cff766851039ed7648d16
David Williams <david@volumesoffun.com>
2013-07-16 23:02:53 +0200

    Tank movement code now respects key mappings.

--- Examples/Unity/Test DLL/Assets/Panther/Demo Scene/MoveTank.js
+++ Examples/Unity/Test DLL/Assets/Panther/Demo Scene/MoveTank.js
@@ -23,27 +23,20 @@ function Start() {
 }
 
 
-function Update () {
+function Update () 
+{
+	var vertical : float = Input.GetAxis ("Vertical");
 	
-	if (Input.GetKey (KeyCode.UpArrow)) {
-		// plus speed
-		if (currentVelocity <= maxSpeed) 
-			currentVelocity += acceleration * Time.deltaTime;
-
-	} else if (Input.GetKey (KeyCode.DownArrow)) {
-		// minus speed
-		if (currentVelocity >= -maxSpeed) 
-			currentVelocity -= acceleration * Time.deltaTime;
-		
-	} else {
+	currentVelocity += acceleration * Time.deltaTime * vertical;
+	
+	if(Mathf.Abs(vertical) < 0.01f)
+	{
 		// No key input. 
 		if (currentVelocity > 0) 
 			currentVelocity -= acceleration * Time.deltaTime;
 		else if (currentVelocity < 0) 
 			currentVelocity += acceleration * Time.deltaTime;
-
 	}
-
 
 	// Turn off engine if currentVelocity is too small. 
 	if (Mathf.Abs(currentVelocity) <= 0.05)
@@ -52,70 +45,8 @@ function Update () {
 	// Move Tank by currentVelocity
 	transform.Translate(Vector3(0, 0, currentVelocity * Time.deltaTime));
 
-	// Move Tracks by currentVelocity	 
-	if (currentVelocity > 0) {
-		// Move forward
-		leftTrack.speed = currentVelocity;
-		leftTrack.GearStatus = 1;
-		rightTrack.speed = currentVelocity;
-		rightTrack.GearStatus = 1;
-	}
-	else if (currentVelocity < 0)	{
-		// Move Backward
-		leftTrack.speed = -currentVelocity;
-		leftTrack.GearStatus = 2;
-		rightTrack.speed = -currentVelocity;
-		rightTrack.GearStatus = 2;
-	}
-	else {
-		// No Move
-		leftTrack.GearStatus = 0;	
-		rightTrack.GearStatus = 0;		
-	}
-
-
-	// Turn Tank
-	if (Input.GetKey (KeyCode.LeftArrow)) {
-		if (Input.GetKey(KeyCode.DownArrow)) {
-			// Turn right
-			transform.Rotate(Vector3(0, rotationSpeed * Time.deltaTime, 0));
-			
-			leftTrack.speed = rotationSpeed;
-			leftTrack.GearStatus = 1;
-			rightTrack.speed = rotationSpeed;
-			rightTrack.GearStatus = 2;
-			
-		} else {
-			// Turn left
-			transform.Rotate(Vector3(0, -rotationSpeed * Time.deltaTime, 0));
-			
-			leftTrack.speed = rotationSpeed;
-			leftTrack.GearStatus = 2;
-			rightTrack.speed = rotationSpeed;
-			rightTrack.GearStatus = 1;
-			
-		}
-	}
-
-	if (Input.GetKey (KeyCode.RightArrow)) {
-		if (Input.GetKey(KeyCode.DownArrow)) {
-			// Turn left
-			transform.Rotate(Vector3(0, -rotationSpeed * Time.deltaTime, 0));
-			leftTrack.speed = rotationSpeed;
-			leftTrack.GearStatus = 2;
-			rightTrack.speed = rotationSpeed;
-			rightTrack.GearStatus = 1;
-
-		} else {
-			// Turn right
-			transform.Rotate(Vector3(0, rotationSpeed * Time.deltaTime, 0));
-			leftTrack.speed = rotationSpeed;
-			leftTrack.GearStatus = 1;
-			rightTrack.speed = rotationSpeed;
-			rightTrack.GearStatus = 2;
-			
-		}
-	}
+	// Turn tank
+	transform.Rotate(Vector3(0, rotationSpeed * Time.deltaTime * Input.GetAxis ("Horizontal"), 0));
 	
 	
 	// Fire!
