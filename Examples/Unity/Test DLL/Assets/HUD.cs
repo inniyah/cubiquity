04fd144f21d0239b947dfe2da3e0ff3a28953ab8
David Williams <david@volumesoffun.com>
2013-07-17 23:35:40 +0200

    Added Cubiquity.dll to Git. Added quit option.

--- Examples/Unity/Test DLL/Assets/HUD.cs
+++ Examples/Unity/Test DLL/Assets/HUD.cs
@@ -15,13 +15,21 @@ public class HUD : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		if(Input.GetKeyDown("escape"))
+		{
+			Application.Quit();
+		}
 	}
 	
 	void OnGUI()
 	{
 		voxelsCollideWithEachOther = GUI.Toggle(new Rect(10, 10, 500, 30), voxelsCollideWithEachOther, "Voxels collide with each other");
 		voxelsCollideWithWorld = GUI.Toggle(new Rect(10, 30, 500, 30), voxelsCollideWithWorld, "Voxels collide with the world");
+		
+		if(GUI.Button(new Rect(10, 60, 200, 30), "Quit"))
+		{
+			Application.Quit();
+		}
 		
 		Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Cube"), LayerMask.NameToLayer("Cube"), !voxelsCollideWithEachOther);
 		Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Cube"), LayerMask.NameToLayer("Volume"), !voxelsCollideWithWorld);
