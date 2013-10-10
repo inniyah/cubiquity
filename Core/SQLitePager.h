cc35f275d6841ae028ba8cc064a93c74bba1ffe2
Daviw Williams <david@volumesoffun.com>
2013-10-10 15:51:18 +0200

    Added initial files for SQLite-based implementation of Pager class.

---
+++ Core/SQLitePager.h
@@ -0,0 +1,18 @@
+#ifndef __CUBIQUITY_SQLITE_PAGER_H__
+#define __CUBIQUITY_SQLITE_PAGER_H__
+
+#include "PolyVoxCore/Impl/TypeDef.h"
+
+#include "PolyVoxCore/Pager.h"
+#include "PolyVoxCore/Region.h"
+
+namespace Cubiquity
+{
+	template <typename VoxelType>
+	class SQLitePager : public PolyVox::Pager<VoxelType>
+	{
+	public:
+	};
+}
+
+#endif //__CUBIQUITY_SQLITE_PAGER_H__
