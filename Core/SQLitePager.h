5534ce426b8ec23999953e2dcf517e3b6d0bb91c
David Williams <david@volumesoffun.com>
2013-10-11 07:41:17 +0200

    Added SQLite files.

--- Core/SQLitePager.h
+++ Core/SQLitePager.h
@@ -6,12 +6,29 @@
 #include "PolyVoxCore/Pager.h"
 #include "PolyVoxCore/Region.h"
 
+#include "SQLite/sqlite3.h"
+
 namespace Cubiquity
 {
+	/**
+	 * Provides an interface for performing paging of data.
+	 */
 	template <typename VoxelType>
 	class SQLitePager : public PolyVox::Pager<VoxelType>
 	{
 	public:
+		/// Constructor
+		SQLitePager(const std::string& dbName);
+
+		/// Destructor
+		virtual ~SQLitePager();
+
+		virtual void pageIn(const Region& region, PolyVox::CompressedBlock<VoxelType>* pBlockData);
+		virtual void pageOut(const Region& region, PolyVox::CompressedBlock<VoxelType>* pBlockData);
+
+	private:
+
+		sqlite3* db;
 	};
 }
 
