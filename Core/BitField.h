66961cffac9bdf8f037b3a720d90522195418256
Daviw Williams <david@volumesoffun.com>
2013-11-01 17:00:36 +0100

    Start of Bitfield class.

---
+++ Core/BitField.h
@@ -0,0 +1,38 @@
+#ifndef CUBIQUITY_BRUSH_H
+#define CUBIQUITY_BRUSH_H
+
+#include <limits>
+
+namespace Cubiquity
+{
+	template <typename StorageType>
+	class BitField
+	{
+	public:
+		StorageType getBits(size_t MSB, size_t LSB)
+		{
+			const size_t totalNoOfBits = sizeof(StorageType) * CHAR_BIT;
+			const size_t noOfBitsToGet = (MSB - LSB) + 1;
+
+			// Build a mask containing all '0's except for the least significant bits (which are '1's).
+			StorageType mask = (std::numeric_limits<StorageType>::max)(); //Set to all '1's
+			mask = mask << noOfBitsToGet; // Insert the required number of '0's for the lower bits
+			mask = ~mask; // And invert
+
+			// Move the desired bits into the LSBs and mask them off
+			StorageType result = (mData >> LSB) & mask;
+
+			return result;
+		}
+
+		void setBits(size_t MSB, size_t LSB, StorageType bitsToSet)
+		{
+			const size_t noOfBitsToSet = (MSB - LSB) + 1;
+		}
+
+	public:
+		StorageType mData;
+	};
+}
+
+#endif //CUBIQUITY_BRUSH_H
