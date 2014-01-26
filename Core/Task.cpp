1327a2557b08efdb2db42e84022afb1ae0ecbb1f
David Williams <david@volumesoffun.com>
2014-01-26 09:42:59 +0100

    Visual Leak Detector seems to cause issues with Unity (probably the garbage collector) so it's now disabled by default.

--- Core/Task.cpp
+++ Core/Task.cpp
@@ -1,6 +1,10 @@
 #include "Task.h"
 
-#include "vld.h"
+// VLD is commented out by default because it interferes with Unity. More specifically, if VLD is enabled then
+// Unity crashes when using a debug version of Cuiquity (VLD is only enabled in debug mode). The crash does not
+// appear to be located in a particular Cubiquity function, and I think it is instead a result of Unity calling
+// the garbage collector and this somehow interferring with whatever VLD does.
+//#include "vld.h"
 
 namespace Cubiquity
 {
