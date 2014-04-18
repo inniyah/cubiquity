81f9bffc2ae970d832e9304c40aaff7d39858109
David Williams <david@volumesoffun.com>
2014-04-18 08:45:09 +0200

    Error handling and fixes for C test case.

--- Wrappers/CubiquityCTest/CubiquityCTest.cpp
+++ Wrappers/CubiquityCTest/CubiquityCTest.cpp
@@ -17,44 +17,49 @@ typedef std::wostringstream tstringstream;
 typedef std::ostringstream tstringstream;
 #endif
 
+void validate(int returnCode)
+{
+	if (returnCode != CU_OK)
+	{
+		std::cout << cuGetErrorCodeAsString(returnCode) << " : " << cuGetLastErrorMessage() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
 int main()
 {
-	//unsigned int volumeID = newColoredCubesVolume(0, 0, 0, 127, 127, 127, 256, 256);
-	
-	//("C:/code/cubiquity/Examples/Volumes/Version 0/VoxeliensTerrain", 32, &volumeID);
 	int width = 128;
 	int height = 32;
 	int depth = 128;
 	unsigned int volumeID;
 	//File name should not alreay exist. 32 is the internal chunk size (not imporant).
-	cuNewEmptyColoredCubesVolume(0, 0, 0, width - 1, height - 1, depth - 1, "NewVolume.vdb", 32, &volumeID);
+	validate( cuNewEmptyColoredCubesVolume(0, 0, 0, width - 1, height - 1, depth - 1, "NewVolume.vdb", 32, &volumeID) );
 
+	CuColor redCol = cuMakeColor(255, 0, 0, 255);
+	CuColor greenCol = cuMakeColor(0, 255, 0, 255);
+	CuColor blueCol = cuMakeColor(0, 0, 255, 255);
 
-	for (int y = 0; y < height; y++)
+	for (int z = 0; z < depth; z++)
 	{
-		std::cout << "Processing slice " << y << std::endl;
-		for (int z = 0; z < depth; z++)
+		for (int y = 0; y < height; y++)
 		{
 			for (int x = 0; x < width; x++)
 			{				
-				CuColor color;
 				if (y < 5)
-					color = cuMakeColor(255, 0, 0, 255);
+					validate( cuSetVoxel(volumeID, x, y, z, redCol) );
 				else if (y < 10)
-					color = cuMakeColor(0, 255, 0, 255);
+					validate( cuSetVoxel(volumeID, x, y, z, greenCol) );
 				else if (y < 15)
-					color = cuMakeColor(0, 0, 255, 255);
-
-				cuSetVoxel(volumeID, x, y, z, color);
+					validate( cuSetVoxel(volumeID, x, y, z, blueCol) );
 			}
 		}
 	}
 
-	cuAcceptOverrideBlocks(volumeID);
+	validate( cuAcceptOverrideBlocks(volumeID) );
 
 	// Retieve the stored voxel to check it worked
 	CuColor storedColor;
-	cuGetVoxel(volumeID, 10, 10, 10, &storedColor);
+	validate( cuGetVoxel(volumeID, 10, 10, 10, &storedColor) );
 
 	// Extract the color components
 	uint8_t red, green, blue, alpha;
@@ -65,7 +70,7 @@ int main()
 
 	std::cout << stros.str();
 
-	cuDeleteColoredCubesVolume(volumeID);	
+	validate( cuDeleteColoredCubesVolume(volumeID) );
 
 	return 0;
 }
