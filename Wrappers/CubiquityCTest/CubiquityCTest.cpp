7f5012bc04f3d6b260286bd3d1436133c25d17f8
David Williams <david@volumesoffun.com>
2014-04-18 08:29:48 +0200

    Better C wrapper example.

--- Wrappers/CubiquityCTest/CubiquityCTest.cpp
+++ Wrappers/CubiquityCTest/CubiquityCTest.cpp
@@ -20,55 +20,52 @@ typedef std::ostringstream tstringstream;
 int main()
 {
 	//unsigned int volumeID = newColoredCubesVolume(0, 0, 0, 127, 127, 127, 256, 256);
+	
+	//("C:/code/cubiquity/Examples/Volumes/Version 0/VoxeliensTerrain", 32, &volumeID);
+	int width = 128;
+	int height = 32;
+	int depth = 128;
 	unsigned int volumeID;
-	cuNewColoredCubesVolumeFromVDB("C:/code/cubiquity/Examples/Volumes/Version 0/VoxeliensTerrain", 32, &volumeID);
-	cuUpdateVolume(volumeID);
-	unsigned int rootOctreeNodeHandle;
-	cuGetRootOctreeNode(volumeID, &rootOctreeNodeHandle);
-	unsigned int noOfVertices;
-	cuGetNoOfVertices(rootOctreeNodeHandle, &noOfVertices);
-	unsigned int noOfIndices;
-	cuGetNoOfIndices(rootOctreeNodeHandle, &noOfIndices);
-	float* pVertices;
-	cuGetVertices(rootOctreeNodeHandle, &pVertices);
-	unsigned int* pIndices;
-	cuGetIndices(rootOctreeNodeHandle, &pIndices);
+	//File name should not alreay exist. 32 is the internal chunk size (not imporant).
+	cuNewEmptyColoredCubesVolume(0, 0, 0, width - 1, height - 1, depth - 1, "NewVolume.vdb", 32, &volumeID);
 
-	tstringstream stros;    
-	stros << noOfVertices << " " << noOfIndices;
-#ifdef _WIN32
-	OutputDebugString(stros.str().c_str());
-#else
-	std::cout << stros;
-#endif
 
-	std::cout << noOfVertices << " " << noOfIndices << std::endl;
+	for (int y = 0; y < height; y++)
+	{
+		std::cout << "Processing slice " << y << std::endl;
+		for (int z = 0; z < depth; z++)
+		{
+			for (int x = 0; x < width; x++)
+			{				
+				CuColor color;
+				if (y < 5)
+					color = cuMakeColor(255, 0, 0, 255);
+				else if (y < 10)
+					color = cuMakeColor(0, 255, 0, 255);
+				else if (y < 15)
+					color = cuMakeColor(0, 0, 255, 255);
 
-	cuDeleteColoredCubesVolume(volumeID);
-
-	/*for(unsigned int ct = 0; ct < noOfIndices; ct++)
-	{   
-		tstringstream stros;    
-		stros << pIndices[ct] << " ";
-		OutputDebugString(stros.str().c_str());
+				cuSetVoxel(volumeID, x, y, z, color);
+			}
+		}
 	}
 
-	tstringstream stros;    
-	stros << std::endl << std::endl;
-	OutputDebugString(stros.str().c_str());*/
+	cuAcceptOverrideBlocks(volumeID);
 
-	/*unsigned int floatsPerVert = 4;
-	for(unsigned int ct = 0; ct < noOfVertices * floatsPerVert; )
-	{
-		tstringstream stros;    
-		stros << pVertices[ct] << " "; ct++;
-		stros << pVertices[ct] << " "; ct++;
-		stros << pVertices[ct] << " "; ct++;
+	// Retieve the stored voxel to check it worked
+	CuColor storedColor;
+	cuGetVoxel(volumeID, 10, 10, 10, &storedColor);
 
-		stros << pVertices[ct] << " "; ct++;
-		stros << std::endl;
-		OutputDebugString(stros.str().c_str());
-	}*/
+	// Extract the color components
+	uint8_t red, green, blue, alpha;
+	cuGetAllComponents(storedColor, &red, &green, &blue, &alpha);
+
+	std::stringstream stros;
+	stros << "Stored voxel color is (" << (int)red << ", " << (int)green << ", " << (int)blue << ", " << (int)alpha << ")" << std::endl;
+
+	std::cout << stros.str();
+
+	cuDeleteColoredCubesVolume(volumeID);	
 
 	return 0;
 }
