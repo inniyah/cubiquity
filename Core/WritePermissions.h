56d53026a350377abf9e2a49e514b1ce0d802335
David Williams <david@volumesoffun.com>
2014-07-06 21:13:08 +0200

    Added missing file.

---
+++ Core/WritePermissions.h
@@ -0,0 +1,14 @@
+#ifndef CUBIQUITY_WRITEPERMISSIONS_H_
+#define CUBIQUITY_WRITEPERMISSIONS_H_
+
+namespace WritePermissions
+{
+	enum WritePermission
+	{
+		ReadOnly,
+		ReadWrite
+	};
+}
+typedef WritePermissions::WritePermission WritePermission;
+
+#endif // CUBIQUITY_WRITEPERMISSIONS_H_
