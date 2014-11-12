a416af54e45e1d3279981151f0e4e281b5cff3d6
David Williams <david@volumesoffun.com>
2014-11-12 16:45:19 +0100

    Tidying code, added scale parameter.

--- Tools/ProcessVDB/ImportHeightmap.cpp
+++ Tools/ProcessVDB/ImportHeightmap.cpp
@@ -130,7 +130,8 @@ bool importHeightmapAsColoredCubesVolume(ez::ezOptionParser& options)
 bool importHeightmapAsTerrainVolume(ez::ezOptionParser& options)
 {
 	// Open the heightmap
-	int originalWidth = 0, originalHeight = 0, originalChannels; // 'Original' values are before resampling
+	int originalWidth = 0, originalHeight = 0, originalChannels = 0; // 'Original' values are before resampling
+	int originalDepth = 256; // Assume 8-bit input image.
 	string heightmapFilename;
 	options.get("-heightmap")->getString(heightmapFilename);
 	unsigned char* originalData = stbi_load(heightmapFilename.c_str(), &originalWidth, &originalHeight, &originalChannels, 0);
@@ -152,24 +153,28 @@ bool importHeightmapAsTerrainVolume(ez::ezOptionParser& options)
 		}
 	}
 
-	int resampledWidth = 512;
-	int resampledHeight = 512;
+	float scale;
+	options.get("-scale")->getFloat(scale);
+
+	if (scale > 0.5f)
+	{
+		std::cout << "Warning - We recommend using '-scale' with a value of 0.5 or less when generating a terrain volume from a heightmap." << std::endl;
+	}
+
+	int resampledWidth = static_cast<int>((originalWidth * scale) + 0.5f);
+	int resampledHeight = static_cast<int>((originalHeight * scale) + 0.5f);
+	int resampledDepth = static_cast<int>((originalDepth * scale) + 0.5f);
 	int resampledPixelCount = resampledWidth * resampledHeight;
 	float* resampledData = new float[resampledPixelCount];
-	/*for (int ct = 0; ct < resampledPixelCount; ct++)
-	{
-		resampledData[ct] = floatOriginalData[ct];
-	}*/
 
 	stbir_resize_float(floatOriginalData, originalWidth, originalHeight, 0,
 		resampledData, resampledWidth, resampledHeight, 0, 1);
 
-	// Create the volume. When importing we treat 'y' as up because most game engines and
-	// physics engines expect this. This means we need to swap the 'y' and 'slice' indices.
+
 	uint32_t volumeHandle;
 	uint32_t volumeWidth = resampledWidth;
-	uint32_t volumeHeight = 64; // Assume we're not loading HDR images, not supported by stb_image anyway
-	uint32_t volumeDepth = resampledHeight;
+	uint32_t volumeHeight = resampledDepth; // Swapping height and depth 
+	uint32_t volumeDepth = resampledHeight; // so that 'y' is 'up'
 	string pathToVoxelDatabase;
 	options.get("-terrain")->getString(pathToVoxelDatabase);
 	if (cuNewEmptyTerrainVolume(0, 0, 0, volumeWidth - 1, volumeHeight - 1, volumeDepth - 1, pathToVoxelDatabase.c_str(), 32, &volumeHandle) != CU_OK)
@@ -177,6 +182,9 @@ bool importHeightmapAsTerrainVolume(ez::ezOptionParser& options)
 		cerr << "Failed to create new empty volume" << endl;
 		return false;
 	}
+
+	// Don't yet know the best value for this.
+	float scalarFieldCompressionFactor = 255.0f;
 
 	for (uint32_t imageY = 0; imageY < resampledHeight; imageY++)
 	{
@@ -193,28 +201,15 @@ bool importHeightmapAsTerrainVolume(ez::ezOptionParser& options)
 
 				float fDiff = fPixelHeight - fHeight;
 
-				fDiff *= 1000.0f;
+				fDiff *= scalarFieldCompressionFactor;
 
 				int diff = int(fDiff);
 				diff += 127;
-
-				/*int diff = (int)height - int(*heightmapPixel);
-				diff *= 10;
-				diff += 127;*/
 
 				diff = (std::min)(diff, 255);
 				diff = (std::max)(diff, 0);
 
 				materialSet.data = static_cast<uint8_t>(diff);
-
-				/*if (height < imageX)
-				{
-					materialSet.data = 255;
-				}
-				else
-				{
-					materialSet.data = 0;
-				}*/
 
 				if (cuSetVoxel(volumeHandle, imageX, height, imageY, &materialSet) != CU_OK)
 				{
--- Tools/ProcessVDB/main.cpp
+++ Tools/ProcessVDB/main.cpp
@@ -73,6 +73,16 @@ int main(int argc, const char* argv[])
 		"--terrain"     // Flag token.
 		);
 
+	options.add(
+		"1.0", // Default.
+		0, // Required?
+		1, // Number of args expected.
+		0, // Delimiter if expecting multiple args.
+		"Scale factor", // Help description.
+		"-scale",     // Flag token.
+		"--scale"     // Flag token.
+		);
+
 	options.parse(argc, argv);
 
 	if (options.isSet("--import"))
